d1eb7aba0978e1553042167fc97bfca113521d54
pjqdyd <1909025079@qq.com>
2019-10-18 13:06:37 +0800

    update

--- wx_test/app.js
+++ wx_test/app.js
@@ -1,39 +1,18 @@
 //app.js
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+  serverUrl: "http://192.168.0.111:8080",
+  userInfo: null,
 
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
+  setGlobalUserInfo: function (user) {
+    //设置用户对象为缓存的方法(key,data)
+    wx.setStorageSync("userInfo", user);
   },
-  globalData: {
-    userInfo: null
-  }
+
+  getGlobalUserInfo: function () {
+    //获取缓存的用户对象
+    return wx.getStorageSync("userInfo");
+  },
+
+  onLaunch: function () { },
 })
--- wx_test/pages/index/index.js
+++ wx_test/pages/index/index.js
@@ -6,46 +6,52 @@ Page({
   data: {},
   onLoad: function() {},
   //获取用户信息1(把加密数据给后端))
-  onGotUserInfo1(value) { //value包含用户信息encryptedData, iv
-    console.log(value)
-    wx.login({ //登录
-      success(res) {
-        console.log(res)
-        //获取res.code, encryptedData, iv数据传给后端
-        wx.request({
-          url: 'http://192.168.0.111:8080/wx/login',
-          method: "POST",
-          header: {
-            'content-type': 'application/x-www-form-urlencoded'
-          },
-          data: {
-            code: res.code,
-            encryptedData: value.detail.encryptedData,
-            iv: value.detail.iv
-          },
-          success(result) {
-            console.log(result);
-          },
-          fail() {
-            console.log("失败");
-          }
-        })
-      },
-      fail() {
-        console.log("登录失败");
-      }
-    })
-  },
+  // onGotUserInfo1(value) { //value包含用户信息encryptedData, iv
+  //   console.log(value)
+  //   wx.login({ //登录
+  //     success(res) {
+  //       console.log(res)
+  //       //获取res.code, encryptedData, iv数据传给后端
+  //       wx.request({
+  //         url: 'http://192.168.0.111:8080/wx/login',
+  //         method: "POST",
+  //         header: {
+  //           'content-type': 'application/x-www-form-urlencoded'
+  //         },
+  //         data: {
+  //           code: res.code,
+  //           encryptedData: value.detail.encryptedData,
+  //           iv: value.detail.iv
+  //         },
+  //         success(result) {
+  //           console.log(result);
+  //         },
+  //         fail() {
+  //           console.log("失败");
+  //         }
+  //       })
+  //     },
+  //     fail() {
+  //       console.log("登录失败");
+  //     }
+  //   })
+  // },
   //获取用户信息2(直接把用户数据传给后端)
   onGotUserInfo2(value) { //value.detail.rawData包含用户信息
+    var that = this;
     console.log(value)
+    wx.showLoading({
+      title: '正在登录'
+    });
+
     wx.login({ //登录
       success(res) {
         console.log(res)
         var userInfo = JSON.parse(value.detail.rawData);
+
         //获取res.code, 用户数据传给后端
         wx.request({
-          url: 'http://192.168.0.111:8080/wx/sample/login?code=' + res.code,
+          url: app.serverUrl + '/wx/login?code=' + res.code,
           method: "POST",
           header: {
             'content-type': 'application/json'
@@ -57,10 +63,32 @@ Page({
             city: userInfo.city,
             province: userInfo.province,
             country: userInfo.country,
-            avatarUrl: userInfo.avatarUrl
+            avatarUrl: userInfo.avatarUrl,
+
+            userId: app.getGlobalUserInfo().userId,
+            token: app.getGlobalUserInfo().token
           },
           success(result) {
-            console.log(result);
+            wx.hideLoading();
+            console.log(result.data);
+            if(result.data.code == 200){ //登录成功
+              wx.showToast({
+                title: '登录成功',
+                icon: 'success',
+                duration: 1000
+              });
+              var user = result.data.data;//返回的用户信息
+              app.userInfo = user;
+              //设置全局的用户信息为本地缓存
+              app.setGlobalUserInfo(user);
+            }else{
+              //登录失败弹出框
+              wx.showToast({
+                title: result.data.msg,
+                icon: 'none',
+                duration: 1000
+              })
+            }
           },
           fail() {
             console.log("失败");
